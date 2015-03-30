2b175562335736a05ba6559b84fd19026cf21f99
Alberto Mendoza <Alberto@MacBook-Pro-de-Alberto.local>
2015-03-30 19:38:23 +0100

    Initial Commit

---
+++ TiempoCanarias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TiempoCanarias
+//
+//  Created by Alberto Mendoza on 30/3/15.
+//  Copyright (c) 2015 Alberto Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
