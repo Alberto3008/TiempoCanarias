893924fe4c5aa7e912d971b004d37f86cff264f3
Alberto Mendoza <Alberto@MacBook-Pro-de-Alberto.local>
2015-03-31 01:35:35 +0100

    Empezando UI

--- TiempoCanarias/ViewController.swift
+++ TiempoCanarias/ViewController.swift
@@ -12,10 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var capturaTexto: UITextField!
     @IBOutlet weak var ciudadPicker: UIPickerView!
+    @IBOutlet weak var climaLabel: UILabel!
+    var clima:String?
     
-    let ciudades = ["Santa Cruz", "La Laguna"]
-    
-    var ciudades2 = [
+    var ciudades = [
         Ciudad(nombre: "La Laguna", id: 2511401),
         Ciudad(nombre: "Las Palmas de Gran Canaria", id: 2515270),
         Ciudad(nombre: "Santa Cruz de Tenerife", id: 2511174),
@@ -33,7 +33,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tiempoBttn(sender: AnyObject) {
-        println(ciudades2[ciudadPicker.selectedRowInComponent(0)].id)
+        println(ciudades[ciudadPicker.selectedRowInComponent(0)].id)
+        tiempoWebService()
 
     }
 
@@ -44,11 +45,11 @@ class ViewController: UIViewController {
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return ciudades2.count
+        return ciudades.count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return ciudades2[row].nombre
+        return ciudades[row].nombre
     }
     
     struct Ciudad
@@ -61,6 +62,59 @@ class ViewController: UIViewController {
             self.id = id
         }
     }
+    
+    
+    func tiempoWebService(){
+       let urlPath = "http://api.openweathermap.org/data/2.5/weather?id=\(ciudades[ciudadPicker.selectedRowInComponent(0)].id)&lang=sp"
+          println(urlPath)
+        let url = NSURL(string: urlPath)
+        
+        let session = NSURLSession.sharedSession()
+        
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+            
+            if(error != nil){
+                //Imprime si hay un error por consola
+                println(error.localizedDescription)
+            }
+            
+           var nsdata:NSData = NSData(data:data)
+            
+           println(nsdata)
+           self.recuperarClima(nsdata)
+            
+            dispatch_async(dispatch_get_main_queue(),{ println(self.clima!); self.climaLabel.text = self.clima!})
+            
+        })
+        
+        task.resume()
+        
+        
+        
+        
+        
+    }
+    
+    func recuperarClima(nsdata:NSData){
+        
+        let jsonCompleto : AnyObject! = NSJSONSerialization.JSONObjectWithData(nsdata, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        
+        
+        println(jsonCompleto)
+        
+        let arregloJsonClima = jsonCompleto["weather"]
+        
+        if let jsonArray = arregloJsonClima as? NSArray{
+            
+            //Recorre por el array de la respues de json del servidor
+            jsonArray.enumerateObjectsUsingBlock({model, index, stop in
+                 self.clima = model["description"] as? String
+            });
+        }
+    }
+    
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
